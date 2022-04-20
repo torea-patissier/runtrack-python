5f6cd729bcd3fed38fa851375933c8c77c5ae142
Toréa Patissier <torea.patissier@laplateforme.io>
2022-04-20 12:06:48 +0200

    jour2 job 0

---
+++ jour1/job19.py
@@ -0,0 +1,11 @@
+askForX = input('Entrez le nombre de colonnes : ')
+# askForY = input('Entrez le nombre de lignes : ')
+
+i = 0
+while i < int(askForX):
+    print('-', end= '')
+    i = i + 1
+# j = 0
+# while j < int(askForY):
+#     print('|', end='')
+#     j = j + 1
---
+++ jour2/job0.py
@@ -0,0 +1,31 @@
+class Personne:
+
+    def __init__(self, nom, prenom):
+        self.nom = nom
+        self.prenom = prenom
+
+    @property
+    def nom(self):
+        return self._nom
+
+    @nom.setter
+    def nom(self, nom):
+        self._nom = nom
+
+    @property
+    def prenom(self):
+        return self._prenom
+
+    @prenom.setter
+    def prenom(self, prenom):
+        self._prenom = prenom
+
+
+    def sePresenter(self):
+        print(self.nom + ' ' + self.prenom)
+
+p1 = Personne('Toréa','Patissier')
+p1.sePresenter()
+
+p2 = Personne('Abdul','Kamara')
+p2.sePresenter()
